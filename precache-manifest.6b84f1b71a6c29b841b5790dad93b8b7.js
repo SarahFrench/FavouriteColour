30aa45872e9ebbb25d8d8e22267b3f7ab552052c
Sarah French <sarah@sarahfrench.co.uk>
2019-07-25 21:43:09 +0100

    Updates

---
+++ precache-manifest.6b84f1b71a6c29b841b5790dad93b8b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187f591e1104da05556b5d1e702dc24b",
+    "url": "/FavouriteColour/index.html"
+  },
+  {
+    "revision": "1c41bc96a271407d15fc",
+    "url": "/FavouriteColour/static/css/main.85ae652b.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/FavouriteColour/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "1c41bc96a271407d15fc",
+    "url": "/FavouriteColour/static/js/main.18576e7f.chunk.js"
+  },
+  {
+    "revision": "7ca655e3d6794a509db6",
+    "url": "/FavouriteColour/static/js/runtime~main.63cebae9.js"
+  }
+]);
